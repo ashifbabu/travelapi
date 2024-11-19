ef6b3836444490cc6b3855d46b90b439b13afbcb
parosh-mahmud <prparosh@gmail.com>
2024-11-19 13:26:35 +0600

    main file router updated

--- app/flight_services/routes/bdfare/__init__.py
+++ app/flight_services/routes/bdfare/__init__.py
@@ -2,4 +2,3 @@ from fastapi import APIRouter
 from .search import router as search_router
 
 router = APIRouter()
-router.include_router(search_router, prefix="/bdfare", tags=["BDFare"])
--- app/flight_services/routes/flyhub/__init__.py
+++ app/flight_services/routes/flyhub/__init__.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
-from .auth import router as auth_router
+
 from .search import router as search_router
 
 router = APIRouter()
-router.include_router(auth_router, prefix="/flyhub", tags=["FlyHub Authentication"])
-router.include_router(search_router, prefix="/flyhub", tags=["FlyHub Flight Search"])
+
+
